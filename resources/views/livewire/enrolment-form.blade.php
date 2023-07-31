24901cf099a3cc637f3d73968ed96f5e44537c0c
ruelperez <karuelflorperezz@gmail.com>
2023-07-31 20:10:01 +0800

    set an instruction in readme file

--- resources/views/livewire/enrolment-form.blade.php
+++ resources/views/livewire/enrolment-form.blade.php
@@ -323,7 +323,7 @@
             </div>
         @elseif($rg == 1)
             <div class="div19">
-                <button type="button" class="btn btn-success" onclick="window.location='{{ route('form.pdf',['user' => $user_id, 'year' => $year_id, 'course' => $course_id, 'sem' => $semester_id])}}'">Preview Form</button>
+                <button type="button" class="btn btn-success" onclick="window.location='{{ route('form.pdf',['user' => $user_id, 'year' => $year_id, 'course' => $course_id, 'sem' => $semester_id])}}'" >Preview Form</button>
             </div>
         @endif
 
